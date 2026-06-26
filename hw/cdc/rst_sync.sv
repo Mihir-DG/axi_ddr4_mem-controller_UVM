04a867a056dc09c6110e4838207743bcc18f6167
Mihir Dasgupta <dasguptamihir10@gmail.com>
2026-06-26 13:28:41 -0400

    async reset synchronized to full system reset

---
+++ hw/cdc/rst_sync.sv
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module rst_sync(
+    input async_rst_n,
+    input clk, // destination clock domain
+    output sync_rst_n
+);
+
+    // tells synthesizer to place these regs together to minimize prop delay
+    (* ASYNC_REG = "TRUE" *) logic q1;
+    (* ASYNC_REG = "TRUE" *) logic q2;
+    (* ASYNC_REG = "TRUE" *) logic q3;
+
+    always_ff @(posedge clk or negedge async_rst_n) begin
+        
+        if (!async_rst_n) begin
+            
+            // if reset asserted
+            q1 <= '0;
+            q2 <= '0;
+            q3 <= '0;
+        
+        end
+
+        else begin
+            
+            // default functioning, rippling high sync_rst_n through 2 FFs for sync
+            q1 <= '1;
+            q2 <= q1;
+            q3 <= q2;
+
+        end
+
+    end
+
+    assign sync_rst_n = q3;    
+
+endmodule
