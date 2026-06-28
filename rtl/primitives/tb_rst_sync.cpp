513e39c0b564801c4f83e50ce11b1851e36543f3
Mihir Dasgupta <dasguptamihir10@gmail.com>
2026-06-27 23:15:29 -0400

    partially done screwing around with verilator learning; saved for morning

--- rtl/primitives/tb_rst_sync.cpp
+++ rtl/primitives/tb_rst_sync.cpp
@@ -10,6 +10,7 @@ vluint64_t sim_time = 0; //used to track when to finish the simulation
 
 int main(int argc, char** argv, char** env) {
 
+    Verilated::commandArgs(argc, argv);
     Vrst_sync *dut = new Vrst_sync; // instantiation;
     // system verilog equivalent --> alu dut(.*);
 
@@ -19,9 +20,13 @@ int main(int argc, char** argv, char** env) {
     dut->trace(m_trace, 5);
     m_trace->open("rst_sync_waveform.vcd");
 
+    dut->clk = 0;
+    dut->async_rst_n = 1;
+
     // SIMULATION MEAT AND BONES RAHHHHH
     while (sim_time < MAX_SIM_TIME){
         dut->clk ^= 1; // inverts clock
+        //dut->async_rst_n = 1;
         dut->eval(); // evalulates all signals module
         m_trace->dump(sim_time); // dumps traced signal values into waveform dump file
         // model is currently only evaluated at clock edges
